f4e523a08764490004edbaf9ea63e1b71f457f08
Carlos Balladares <carlosbf@protonmail.com>
2021-02-23 12:04:01 +0000

    Adding dependencies for testing

--- dishes/remy-web-components/stencil.config.ts
+++ dishes/remy-web-components/stencil.config.ts
@@ -2,6 +2,13 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'remy-web-components',
+  testing: {
+    /**
+     * Gitlab CI doesn't allow sandbox, therefor this parameters must be passed to your Headless Chrome
+     * before it can run your tests
+     */
+    browserArgs: ['--no-sandbox', '--disable-setuid-sandbox'],
+  },
   outputTargets: [
     {
       type: 'dist',
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,4 @@
-cd packages/remy-contracts && yarn build
+cd dishes/remy-contracts && yarn build
+cd ../remy-crepe-mobile && yarn build
 cd ../remy-common && yarn build
+cd ../remy-web-components && yarn build
